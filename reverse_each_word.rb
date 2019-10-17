2d9b5846dfe261c1888ba9bfead28fb972c7dd6e
Alex Martinez <alexandr3martin3z@gmail.com>
2019-10-17 22:45:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,9 +4,9 @@ def reverse_each_word(string)
   words = string.split
   new_sentence = []
   words.each do |word|
-    return "#{word.reverse}"
-  binding.pry
-  #reverse_sentence = []
-  #words.each do |word|
+    new_sentence << "#{word.reverse}"
+    binding.pry
+  end
+  new_sentence.join
 
 end
