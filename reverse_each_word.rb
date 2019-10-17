42b56de7b84885d52800df1b8c94977c229296f8
Alex Martinez <alexandr3martin3z@gmail.com>
2019-10-17 22:54:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def reverse_each_word(string)
   words = string.split
-  binding.pry
+  #binding.pry
   new_sentence = []
   words.each do |word|
     new_sentence << "#{word.reverse}"
